7e5e9caaa97545e6c0026d09f4e3c679fd00c698
Eduardo Alvarado <e.alvaradod@gmail.com>
2016-02-23 00:19:00 -0600

    first commit

--- Swipe & Shakes/ViewController.swift
+++ Swipe & Shakes/ViewController.swift
@@ -7,12 +7,57 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer = AVAudioPlayer()
+
+    var sounds = ["belch", "giggle", "pew", "pig", "snore", "uuu"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: "swiped:")
+        swipeRight.direction = UISwipeGestureRecognizerDirection.Right
+        self.view.addGestureRecognizer(swipeRight)
+
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: "swiped:")
+        swipeRight.direction = UISwipeGestureRecognizerDirection.Up
+        self.view.addGestureRecognizer(swipeUp)
+
+
+    }
+
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        if event?.subtype == UIEventSubtype.MotionShake {
+            print("Device was shaken")
+
+            let randomNumber = Int(arc4random_uniform(UInt32(sounds.count)))
+
+            let fileLocation = NSBundle.mainBundle().pathForResource(sounds[randomNumber], ofType: "mp3")
+
+
+            do{
+                try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: fileLocation!))
+                player.play()
+            } catch{
+                print("Error playing the file: " + fileLocation!)
+            }
+        }
+    }
+
+    func swiped(gesture: UIGestureRecognizer){
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer{
+            switch swipeGesture.direction {
+                case UISwipeGestureRecognizerDirection.Right:
+                    print("Swipe right")
+                case UISwipeGestureRecognizerDirection.Up:
+                    print("Swipe up")
+                default:
+                    break
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
